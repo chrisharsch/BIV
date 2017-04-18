3605c7a7d97085c2f07eb718d0700629f02e03b5
eikood <hanfaplast@googlemail.com>
2017-04-18 14:08:50 +0200

    alles richtig

--- BIV_BLATT_2.m
+++ BIV_BLATT_2.m
@@ -18,11 +18,18 @@ title('Histogramm');
 hsi_bluete = rgb2hsi(imread('blueten.jpg'));
 hsi_channel_3 = hsi_bluete(:,:,3);
 hist_brightness = imhist(hsi_channel_3);
-plot(hist_brightness, 'y');
+plot(hist_brightness, 'black');
 % HSI liefert die Intensität der Farbkanäle und durch das Histogramm wird
 % diese dargestellt
 %% Blatt 2 Aufgabe 1d
 hsi_bluete = rgb2hsi(imread('blueten.jpg'));
 hsi_bluete_channel_3 = hsi_bluete(:,:,3);
-min_brightness = min(hsi_bluete_channel_3(:))
-max_brightness = max(hsi_bluete_channel_3(:))
+min_brightness = min(hsi_bluete_channel_3(:));
+max_brightness = max(hsi_bluete_channel_3(:));
+%% Blatt 2 Aufgabe 1e
+gray_blueten = rgb2gray(imread('blueten.jpg'));
+Low_High = stretchlim(gray_blueten);
+%% Blatt 2 Aufgabe 1f
+hist_minmax = imhist(Low_High);
+plot(hist_minmax, 'blue');
+
